e289f9b1f0ef44ff28303ee7ff729dda04dc4cd9
LucStk <luc.sterkers@laposte.net>
2025-05-19 23:19:13 +0200

    use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./landing/templatetags/*.html",
+    "./landing/templates/**/*.html",
+    "./static/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
